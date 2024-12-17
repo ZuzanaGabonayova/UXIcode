aff762464f4af032b4ea0b3c03ab7bb84ecb4f53
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2024-12-17 21:53:13 +0100

    created default layout

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <NavbarComponent />
+      <Nuxt />
+    </div>
+</template>
+
+
+<script>
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import NavbarComponent from '../components/NavbarComponent.vue'
+
+export default {
+    components: {
+      NavbarComponent
+    }
+}
+
+</script>
