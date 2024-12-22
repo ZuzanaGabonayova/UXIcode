db6b916a1f9315715fe1b9d77432922e5b6660de
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2024-12-22 23:54:54 +0100

    added components to be displayed on services subpage

---
+++ pages/services.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <MainBackgroundComponent
+      :img-src="require('../assets/background/services.jpg')"
+      heading="Services"
+    />
+
+    <ServicesComponent />
+    <AboutServicesComponent />
+
+  </div>
+</template>
+
+<script>
+import MainBackgroundComponent from '../components/MainBackgroundComponent.vue';
+import ServicesComponent from '../components/ServicesComponent.vue';
+import AboutServicesComponent from '../components/AboutServicesComponent.vue';
+
+export default {
+    components: {
+        MainBackgroundComponent,
+        ServicesComponent,
+        AboutServicesComponent
+    }
+}
+</script>
