6004bed7a08b1b34d30fae0ad0d492857ca4509c
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2024-12-22 23:53:40 +0100

    default layout for each page

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,18 +2,30 @@
     <div>
       <NavbarComponent />
       <Nuxt />
+      <FooterComponent />
     </div>
 </template>
 
 
 <script>
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import NavbarComponent from '../components/NavbarComponent.vue'
+import FooterComponent from '../components/FooterComponent.vue'
 
 export default {
     components: {
-      NavbarComponent
+      NavbarComponent,
+      FooterComponent
     }
 }
 
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=K2D:wght@300;400;500;600;700&display=swap');
+
+/* Apply the K2D font globally */
+* {
+  font-family: 'K2D', sans-serif;
+}
+
+</style>
