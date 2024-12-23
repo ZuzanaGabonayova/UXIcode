7ee69fb4c73a0fae32a26b6b890113bda35dd983
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2024-12-23 19:00:36 +0100

    imported components into about page

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <MainBackgroundComponent
+      :img-src="require('../assets/background/aboutus.jpg')"
+      heading="About us"
+    />
+
+    <AboutUsComponent />
+  </div>
+</template>
+
+<script>
+import MainBackgroundComponent from '../components/MainBackgroundComponent.vue'
+import AboutUsComponent from '../components/AboutUsComponent.vue';
+
+export default {
+  components: {
+    MainBackgroundComponent,
+    AboutUsComponent
+  },
+}
+</script>
