d6f4ed5c4f19d4e6fbb58ef9b6e09342e03e99a5
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2024-12-28 15:45:44 +0100

    server side of website initialized

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const contactRoutes = require('./routes/Contact');
+const adminRoutes = require('./routes/Admin');
+
+require('dotenv').config()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+// Middleware
+app.use(cors({
+  origin: 'http://localhost:3000', // Allow requests from frontend
+  methods: 'GET,POST,DELETE', // Specify allowed methods
+}));
+app.use(bodyParser.json())
+
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((error) => console.error('MongoDB connection error:', error))
+
+
+// API Route
+app.use('/api/contact', contactRoutes);
+
+app.use('/api/admin', adminRoutes);
+
+
+// Start Server
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+)
